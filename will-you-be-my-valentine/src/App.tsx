000e145ca6289ecd8c6d8e4019b055731b2a9726
RYangunawAN <ryangunawan.we@gmail.com>
2025-02-04 21:07:39 +0700

    project init

---
+++ will-you-be-my-valentine/src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/General.css";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ will-you-be-my-valentine/src/components/Home.tsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+import "../styles/Home.css";
+
+const Home = () => {
+    return (
+        <section className="home" id="home">
+            <Container>
+                <p>Test</p>
+            </Container>
+        </section>
+    );
+};
+
+export default Home;
